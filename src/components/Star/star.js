f5b3a203098e010466a5d06625c1dee3ec0c530b
Egor <godovanishe@gmail.com>
2022-07-23 14:33:22 +0300

    added single static start component

---
+++ src/components/Star/star.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './star.css';
+
+export const Star = () => {
+    return (
+        <div className="star-head">
+            <p>1</p>
+            <p>Name</p>
+            <div className="star"></div>
+        </div>
+    )
+}
