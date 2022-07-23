6eeedea948bdf718f8a71cc4adab6de058eadae6
Egor <godovanishe@gmail.com>
2022-07-23 15:33:18 +0300

    added mapping to render

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import { Star } from './components/Star/star';
 
+const productArray = [
+  {
+    id: 2,
+    name: 'Product 2',
+    isFilled: true
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    isFilled: true
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    isFilled: false
+  }
+];
+
+
 function App() {
+
+let newArray = productArray.map(product => (<Star id={product.id} name={product.name} isFilled={product.isFilled}/>));
+
   return (
     <div className="App">
-      <Star id={2} name='Product' isFilled={false}/>
+      {newArray}
     </div>
   );
 }
